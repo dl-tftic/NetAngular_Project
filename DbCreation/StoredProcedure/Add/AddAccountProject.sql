435ab4840823c098e802f73cae586bc9f8a4633f
DL <d.lissens@stag.technofuturtic.education>
2021-01-11 17:00:02 +0100

    Add AddAccountProject

---
+++ DbCreation/StoredProcedure/Add/AddAccountProject.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[AddAccountProject]
+	@AccountId int,
+	@ProjectId int
+AS
+	DECLARE @rtn int;
+
+	INSERT INTO [Account_Project] ([AccountId], [ProjectId])
+	VALUES (@AccountId, @ProjectId);
+
+	SET @rtn = SCOPE_IDENTITY();
+	SELECT @rtn;
+	RETURN @rtn;
