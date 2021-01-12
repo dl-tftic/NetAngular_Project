6e12de0dffa86e5f4cc35660700bf78653cee50e
DL <d.lissens@stag.technofuturtic.education>
2021-01-12 14:20:10 +0100

    add

---
+++ DbCreation/StoredProcedure/Get/GetProductCategories.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetProductCategories]
+	@productId int
+AS
+	SELECT
+		[Product_Category].[Id],
+		[Product_Category].[CategoryId],
+		[Product_Category].[ParentCategoryProductId],
+		[Product_Category].[ProductId]
+	FROM
+		[Product_Category]
+	WHERE
+		[Product_Category].[Id] = @productId
--- DbCreation/StoredProcedure/Get/GetProjectCategories.sql
+++ DbCreation/StoredProcedure/Get/GetProjectCategories.sql
@@ -2,16 +2,11 @@
 	@projectId int
 AS
 	SELECT
-		[Category].[Id],
-		[Category].[Name],
-		[Category].[Description],
-		[Category].[Type],
-		[Category].[CreateDate],
-		[Category].[CreateBy],
+		[Project_Category].[Id],
+		[Project_Category].[CategoryId],
 		[Project_Category].[ParentProjectCategoryId],
 		[Project_Category].[ProjectId]
 	FROM
-		[Category]
-	INNER JOIN [Project_Category] ON [Project_Category].[CategoryId] = [Category].[Id]
-	INNER JOIN [Project] ON [Project_Category].[ProjectId] = [Project].[Id]
-	AND [Project].[Id] = @projectId
+		[Project_Category]
+	WHERE
+		[Project_Category].[Id] = @projectId
