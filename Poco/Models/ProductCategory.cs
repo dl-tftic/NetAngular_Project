042329b73958d8fb6181238eb564f494c32ba74b
DL <d.lissens@stag.technofuturtic.education>
2021-01-12 14:10:02 +0100

    Add ProjectCategory & ProductCategory

---
+++ Poco/Models/ProductCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO.Models
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int ProductId { get; set; }
+        public int ParentCategoryProductId { get; set; }
+    }
+}
---
+++ Poco/Models/ProjectCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO.Models
+{
+    public class ProjectCategory
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int ProductId { get; set; }
+        public int ParentProjectCategoryId { get; set; }
+    }
+}
