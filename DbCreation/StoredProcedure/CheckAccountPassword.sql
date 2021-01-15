c7ed2a4b9a8c6ebc4ae1a8746177d1749e803152
DL <d.lissens@stag.technofuturtic.education>
2021-01-15 10:10:05 +0100

    update CheckAccountPassword

--- DbCreation/StoredProcedure/CheckAccountPassword.sql
+++ DbCreation/StoredProcedure/CheckAccountPassword.sql
@@ -2,8 +2,11 @@
 	@login varchar(80),
 	@password varchar(50)
 AS
-	SELECT [ID]
-	FROM [Account]
-	WHERE [Login] = @login
+	SELECT 
+		[Id]
+	FROM 
+		[Account]
+	WHERE 
+		[Login] = @login
 	AND [Password] = HASHBYTES('SHA2_256', CONCAT(@password, [Salt]));
 
